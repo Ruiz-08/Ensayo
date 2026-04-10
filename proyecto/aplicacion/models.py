ed243295f8470995525cb0fe16971e0787986da4
Ruiz <anamruizr3@gmail.com>
2026-04-10 09:52:18 -0500

    Integracion de carpetas

--- proyecto/aplicacion/models.py
+++ proyecto/aplicacion/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Equipo(models.Model):
     nombre = models.CharField(max_length=50)
+    logo_url = models.URLField(max_length=500, null=True, blank=True)
+    background_url = models.URLField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return self.nombre
@@ -9,6 +11,7 @@ class Equipo(models.Model):
 class Jugador(models.Model):
     nombre = models.CharField(max_length=50)
     equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE, related_name='jugadores')
+    foto_url = models.URLField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return f"{self.nombre} ({self.equipo.nombre})"
